6c491d48585c2ad39d660594eebef10b96610d3f
Barnalibonnu <95023497+Barnalibonnu@users.noreply.github.com>
2025-03-09 03:56:10 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from improved-scriptV2.py import TokenManager, APIClient, WebServer
+from improvedscriptV2.py import TokenManager, APIClient, WebServer
 
 def create_app():
     # Initialize from environment variables
